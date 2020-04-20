ad74a3b4eac425326bd73dd611ccf44775b18e96
Satyanarayana Vanukuru <satyavanu@gmail.com>
2020-04-20 09:31:37 +0200

    updated code

--- src/services/SmartCheckWinStrategy.js
+++ src/services/SmartCheckWinStrategy.js
@@ -1,67 +1,65 @@
-const strategy = ({ board, r, c, player }) => {
-  let discs = 0
-  for (let i = 0; i < board[r].length; ++i) {
-    discs = board[r][i] == player ? discs + 1 : 0
-    if (discs >= 4) {
-      console.log('player won', player)
-      return player
+const strategy = ({ board }) => {
+  const columns = getColumns() // alte and flip
+  const diags = getDiags(board) // for diagonal
+  let winner = 0 // none wins
+
+  for (let i = 0; i < board.length; ++i) {
+    const isHorizontalMatch = board[i].join('').match(/1{4}|2{4}/)
+    if (isHorizontalMatch) {
+      isHorizontalMatch[0].indexOf('1') > -1 ? (winner = '1') : (winner = '2')
     }
   }
 
-  //different rows, same column = vertically
-  discs = 0
-  for (let j = 0; j < board.length; ++j) {
-    discs = board[j][c] == player ? discs + 1 : 0
-    if (discs >= 4) {
-      console.log('player won', player)
-      return player
+  for (let j = 0; j < columns.length; j++) {
+    const iscolMatch = columns[j].join('').match(/1{4}|2{4}/)
+    if (iscolMatch) {
+      iscolMatch[0].indexOf('1') > -1 ? (winner = '1') : (winner = '2')
     }
   }
+  for (let l = 0; l < diags.length; l++) {
+    const diagMatch = diags[l].join('').match(/1{4}|2{4}/)
+    if (diagMatch) {
+      diagMatch[0].indexOf('1') > -1 ? (winner = '1') : (winner = '2')
+    }
+  }
+  
+  return winner
 
-  // diagonally from current chip to bottom left ...
-  discs = 1
-  for (
-    let i = 1;
-    r - i >= 0 && c - i >= 0 && board[r - i][c - i] == player;
-    ++i
-  ) {
-    discs++
-  }
-  // ... and from current chip to top right
-  for (
-    let i = 1;
-    r + i < board.length &&
-    c + i < board[r + i].length &&
-    board[r + i][c + i] == player;
-    ++i
-  ) {
-    discs++
-  }
-  if (discs >= 4) {
-    console.log('player won', player)
-    return player
+  function getColumns() {
+    const cols = []
+    for (let j = 0; j < board[0].length; j++) {
+      const col = []
+      for (let k = 0; k < board.length; k++) {
+        col.push(board[k][j])
+      }
+      cols.push(col)
+    }
+    return cols
   }
 
-  // diagonally from current chip to bottom right ...
-  discs = 1
-  for (
-    let i = 1;
-    r - i >= 0 && c + i < board[r - i].length && board[r - i][c + i] == player;
-    ++i
-  ) {
-    discs++
-  }
-  // ... and from current chip to top left
-  for (
-    let i = 1;
-    r + i < board.length && c - i >= 0 && board[r + i][c - i] == player;
-    ++i
-  ) {
-    discs++
-  }
-  if (discs >= 4) {
-    console.log('player won', player)
-    return player
+  function getDiags(arr) {
+    const diags = []
+    for (let i = -5; i < 7; i++) {
+      const group = []
+      for (let j = 0; j < 6; j++) {
+        if (i + j >= 0 && i + j < 7) {
+          group.push(arr[j][i + j])
+        }
+      }
+      diags.push(group)
+    }
+    for (let i = 0; i < 12; i++) {
+      const group = []
+      for (let j = 5; j >= 0; j--) {
+        if (i - j >= 0 && i - j < 7) {
+          group.push(arr[j][i - j])
+        }
+      }
+      diags.push(group)
+    }
+    return diags.filter(function(a) {
+      return a.length > 3
+    })
   }
 }
 
